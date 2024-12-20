6e936a23b95538afbf22ad0bca8f0f46fc867ecc
dhanbdrkarki1 <dhanbdrkarki111@gmail.com>
2024-12-20 08:25:25 +0545

    docker compose updated

--- blogsite/settings/prod.py
+++ blogsite/settings/prod.py
@@ -45,53 +45,60 @@ MIDDLEWARE = [
 
 TIME_ZONE = 'Asia/Kathmandu'
 
-# AWS S3 settings
-if os.environ.get('AWS_ACCESS_KEY_ID') or os.environ.get('AWS_SECRET_ACCESS_KEY'):
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_FILE_OVERWRITE = False
-# S3 Security Settings
-AWS_DEFAULT_ACL = None  # Don't override file permissions
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',  # 24 hours cache
-}
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-# Storage os.environ.geturation
-STORAGES = {
-    # Media files storage (user uploads)
-    'default': {
-        'BACKEND': 'storages.backends.s3.S3Storage',
-        'OPTIONS': {
-            'bucket_name': AWS_STORAGE_BUCKET_NAME,
-            'custom_domain': AWS_S3_CUSTOM_DOMAIN,
-            'location': 'media',  # Store media files in /media/ directory
-            'file_overwrite': False,  # Don't overwrite files with same name
-        },
-    },
-    # Static files storage
-    'staticfiles': {
-        'BACKEND': 'storages.backends.s3.S3StaticStorage',
-        'OPTIONS': {
-            'bucket_name': AWS_STORAGE_BUCKET_NAME,
-            'custom_domain': AWS_S3_CUSTOM_DOMAIN,
-            'location': 'static',  # Store static files in /static/ directory
-        },
-    },
-}
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# CORS headers for font files
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*'
-}
+# # AWS S3 settings
+# if os.environ.get('AWS_ACCESS_KEY_ID') or os.environ.get('AWS_SECRET_ACCESS_KEY'):
+#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# AWS_S3_FILE_OVERWRITE = False
+# # S3 Security Settings
+# AWS_DEFAULT_ACL = None  # Don't override file permissions
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',  # 24 hours cache
+# }
 
-# URLs for static and media files
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+# # Storage os.environ.geturation
+# STORAGES = {
+#     # Media files storage (user uploads)
+#     'default': {
+#         'BACKEND': 'storages.backends.s3.S3Storage',
+#         'OPTIONS': {
+#             'bucket_name': AWS_STORAGE_BUCKET_NAME,
+#             'custom_domain': AWS_S3_CUSTOM_DOMAIN,
+#             'location': 'media',  # Store media files in /media/ directory
+#             'file_overwrite': False,  # Don't overwrite files with same name
+#         },
+#     },
+#     # Static files storage
+#     'staticfiles': {
+#         'BACKEND': 'storages.backends.s3.S3StaticStorage',
+#         'OPTIONS': {
+#             'bucket_name': AWS_STORAGE_BUCKET_NAME,
+#             'custom_domain': AWS_S3_CUSTOM_DOMAIN,
+#             'location': 'static',  # Store static files in /static/ directory
+#         },
+#     },
+# }
 
-# tell Django where to look for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# # CORS headers for font files
+# AWS_HEADERS = {
+#     'Access-Control-Allow-Origin': '*'
+# }
+
+# # URLs for static and media files
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# # tell Django where to look for static files
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
